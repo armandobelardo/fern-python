61e7b409d35ca87d1dab9026cb533752c1964950
Armando Belardo <armandoubelardo@gmail.com>
2023-08-21 09:23:45 -0400

    use referenced request types instead of inline

--- src/fern_python/generators/fastapi/service_generator/endpoint_parameters/request/file_upload_request_endpoint_parameter.py
+++ src/fern_python/generators/fastapi/service_generator/endpoint_parameters/request/file_upload_request_endpoint_parameter.py
@@ -1,9 +1,7 @@
-from typing import override
-
 import fern.ir.resources as ir_types
-
+from fern_python.codegen import AST
 from ....context import FastApiGeneratorContext
-from .inlined_request_endpoint_parameter import InlinedRequestEndpointParameter
+from .referenced_request_endpoint_parameter import ReferencedRequestEndpointParameter
 from .request_endpoint_parameter import RequestEndpointParameter
 
 
@@ -12,27 +10,27 @@ class FileUploadRequestEndpointParameter(RequestEndpointParameter):
         super().__init__(context=context)
         self._property = property
 
-    @override
     def get_name(self) -> str:
-        return self._property.key
+        return self._property.key.name.original_name
 
-    @override
     def _get_unsafe_name(self) -> str:
-        return self._property.key
+        return self._property.key.name.original_name
 
-    def get_type(self) -> list[RequestEndpointParameter]:
+    def get_type(self) -> AST.TypeHint:
         return self._context.pydantic_generator_context.get_type_hint_for_file_upload()
 
 
 def get_file_upload_request_parameters(
     context: FastApiGeneratorContext, request: ir_types.FileUploadRequest
 ) -> list[RequestEndpointParameter]:
-    return map(
-        lambda property: property.visit(
-            file=lambda prop: FileUploadRequestEndpointParameter(context=context, property=prop),
-            body_property=lambda prop: InlinedRequestEndpointParameter(
-                context=context, service_name=prop, request=request
+    return list(
+        map(
+            lambda property: property.visit(
+                file=lambda prop: FileUploadRequestEndpointParameter(context=context, property=prop),
+                body_property=lambda prop: ReferencedRequestEndpointParameter(
+                    context=context, request_type=prop.value_type
+                ),
             ),
-        ),
-        request.properties,
+            request.properties,
+        )
     )
