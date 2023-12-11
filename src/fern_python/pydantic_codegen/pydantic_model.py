43e7f4aade118532fa93847fc2019b9a6ebaa848
Armando Belardo <armandoubelardo@gmail.com>
2023-12-10 22:31:35 -0500

    replace carriage returns as well

--- src/fern_python/pydantic_codegen/pydantic_model.py
+++ src/fern_python/pydantic_codegen/pydantic_model.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import re
 
 import dataclasses
 from types import TracebackType
@@ -62,7 +63,6 @@ class PydanticModel:
         return self._local_class_reference
 
     def add_field(self, unsanitized_field: PydanticField) -> None:
-
         field = (
             dataclasses.replace(unsanitized_field, name=f"{unsanitized_field.name}_")
             if unsanitized_field.name in BASE_MODEL_PROPERTIES
@@ -349,7 +349,6 @@ def get_field_name_initializer(
     default_factory: Optional[AST.Expression],
     description: Optional[str],
 ) -> Union[AST.Expression, None]:
-
     if alias is None and default_factory is None and description is None:
         return default
 
@@ -376,7 +375,7 @@ def get_field_name_initializer(
             if arg_present:
                 writer.write(", ")
             arg_present = True
-            lines = description.split("\n")
+            lines = re.split("[\n|\r]", description)
             if len(lines) > 1:
                 writer.write_line("description=(")
                 for i, line in enumerate(lines):
