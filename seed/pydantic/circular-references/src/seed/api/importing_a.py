0e0795724e642078b0e9c386525b8aba53e66a08
Armando Belardo <armandoubelardo@gmail.com>
2023-12-18 08:28:37 -0500

    actually run the non-sdk tests

--- seed/pydantic/circular-references/src/seed/api/importing_a.py
+++ seed/pydantic/circular-references/src/seed/api/importing_a.py
@@ -3,8 +3,8 @@
 import datetime as dt
 import typing
 
+from .a.a import A
 from .core.datetime_utils import serialize_datetime
-from .resources.a.a import A
 
 try:
     import pydantic.v1 as pydantic  # type: ignore
